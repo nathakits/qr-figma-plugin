d9cee0bc45b2f362b9631d14b5f8a9fd2e92cf3e
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-03-07 03:15:29 +0700

    update figma message

--- UI/src/helpers/figma-messages.js
+++ UI/src/helpers/figma-messages.js
@@ -1,23 +1,10 @@
 // All Figma Plugin Messages
-
-const createRectangles = (count) => {
-  parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-}
-
-const closePlugin = () => {
-  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-}
-
 const notify = (message, timeout) => {
   parent.postMessage({ pluginMessage: { type: 'msg', message, timeout } }, '*')
 }
 
-const createImage = (buffer, size) => {
-  parent.postMessage({ pluginMessage: { type: 'create-image', buffer, size } }, '*')
+const createQRCode = (svgPath, size) => {
+  parent.postMessage({ pluginMessage: { type: 'create-qr-code', svgPath, size } }, '*')
 }
 
-const createImageArray = (array, size) => {
-  parent.postMessage({ pluginMessage: { type: 'create-image-array', array, size } }, '*')
-}
-
-export { createRectangles, closePlugin, notify, createImage, createImageArray }
+export { notify, createQRCode }
