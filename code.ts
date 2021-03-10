290fe49cb1fb73c34c2fc275704488d773f684cd
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-03-11 00:26:20 +0700

    update ui size wrap QR code vector in frame

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 450, height: 650 });
+figma.showUI(__html__, { width: 400, height: 600 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -19,17 +19,48 @@ figma.ui.onmessage = msg => {
     var currentSel = figma.currentPage.selection
     if (currentSel.length === 0) {
       // insert fill to node
+      let frame = figma.createFrame()
       let vector = figma.createVector()
       let viewport = figma.viewport.center
-      vector.x = viewport.x
-      vector.y = viewport.y
+      let padding = 16
+      // set frame
+      frame.fills = [
+        {
+          type: 'SOLID',
+          color: {
+            r: 1,
+            g: 1,
+            b: 1
+          }
+        }
+      ]
+      frame.name = 'QR Code'
+      frame.resize(msg.size.width + (padding * 2), msg.size.height + (padding * 2))
+      frame.x = viewport.x
+      frame.y = viewport.y
+      frame.appendChild(vector)
+      // set vector
+      vector.vectorPaths = [
+        {
+          windingRule: "NONZERO",
+          data: msg.svgPath
+        }
+      ]
+      vector.fills = [
+        {
+          type: 'SOLID',
+          color: {
+            r: 0,
+            g: 0,
+            b: 0
+          }
+        }
+      ]
       vector.resize(msg.size.width, msg.size.height)
-      console.log(typeof msg.svgPath);
-      vector.vectorPaths = [{
-        windingRule: "EVENODD",
-        data: `${msg.svgPath}`,
-      }]
-      figma.currentPage.appendChild(vector);
+      vector.strokes = []
+      vector.x = padding
+      vector.y = padding
+      figma.currentPage.appendChild(frame);
     }
     // else create a new rectangle and add to page
     else {
